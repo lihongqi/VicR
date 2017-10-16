9acd65131290f918c639dc2448991ab7c32cbbf8
tanszhe <1018595261@qq.com>
2017-10-16 15:48:26 +0800

    数据库操作基本好了

--- TestApp/index.php
+++ TestApp/index.php
@@ -5,16 +5,18 @@
  * Date: 17/10/8
  * Time: 下午5:45
  */
-define('VIC_APP_PATH',__DIR__);
+define('VIC_APP_PATH', __DIR__);
 require_once VIC_APP_PATH . '/../VicR/VicR.php';
 
 App::$start_time = microtime(true);
 
 App::loadRouter();
 
-try{
+try {
     Router::exec();
-}catch (Exception $e){
+} catch (\Except\DbError $e) {
+    echo 'DB error.';
+} catch (Exception $e) {
     echo $e->getMessage();
 }
 
